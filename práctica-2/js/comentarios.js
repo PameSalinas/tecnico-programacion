c3f8e45c7730ec25968a7b09810b43780fd1e089
PameSalinas <salinascrisostomopamelaelizabe@gmail.com>
2025-07-01 16:11:15 -0600

    practica 2 finalizada

--- práctica-2/js/comentarios.js
+++ práctica-2/js/comentarios.js
@@ -1,96 +1,101 @@
-const EMAILJS_PUBLIC_KEY="WdLWYWGRdJ4ylsCbo";
-const EMAILJS_SERVICE_ID="service_bbb7obo";
-const EMAILJS_TEMPLATE_ID="template_l2uyp3w";
+const EMAILJS_PUBLIC_KEY = "ofiYvHbvXfl8inOdA";
+const EMAILJS_SERVICE_ID = "service_dkgp36j";
+const EMAILJS_TEMPLATE_ID = "template_w74tvgq";
 
-EMAILJS_PUBLIC_KEY.init(EMAILJS_PUBLIC_KEY);
+emailjs.init(EMAILJS_PUBLIC_KEY);
 
-const sesionesComentarios = document.querySelectorAll(".comentarios[data-publicaciones-id]");
+const sesionesComentarios = document.querySelectorAll(
+  ".comentarios[data-publicacion-id]"
+);
 
 sesionesComentarios.forEach((sesion) => {
-const publicacionesID =sesion.CDATA_SECTION_NODE.publicacionesId;
-const formulario = sesion.querySelector(".formulario-comentarios");
-const lista = sesion.querySelector(".Lista-comentarios");
-const STORAGE_KEY = `comentarios_${publicacionesID}`;
+  const publicacionID = sesion.dataset.publicacionId;
+  const formulario = sesion.querySelector(".formulario-comentario");
+  const lista = sesion.querySelector(".lista-comentarios");
+  const STORAGE_KEY = `comentarios_${publicacionID}`;
 
-const btnOcultar =sesion.querySelector("button");
-if (btnOcultar && lista) {
+  const btnOcultar = sesion.querySelector("button");
+  if (btnOcultar && lista) {
     btnOcultar.addEventListener("click", () => {
-        lista.classList.toggle("lista-comentarios-oculto");
-        btnOcultar.textContent = lista.classList.contains("lista-comentarios-oculto") 
-        
+      lista.classList.toggle("lista-comentarios-oculto");
+      btnOcultar.textContent = lista.classList.contains(
+        "lista-comentarios-oculto"
+      )
         ? "Mostrar"
-         : "Ocultar";
+        : "Ocultar";
     });
-}
+  }
 
-const cargarComentarios = () => {
-    return JSON.parse(localstorage.getItem(STORRAGE_KEY)) || [];
-};
+  const cargarComentarios = () => {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+  };
 
-const guardarComentarios = (comments) => {
-    return localstorage.setItem(STORAGE_KEY, JSON.stringify(commments));
-};
+  const guardarComentarios = (comments) => {
+    return localStorage.setItem(STORAGE_KEY, JSON.stringify(comments));
+  };
 
-const mostrarComentarios =() => {
-const comentarios =cargarComentarios();
+  const mostrarComentarios = () => {
+    const comentarios = cargarComentarios();
 
-lista.innerHTML=""
-comentarios.forEach((comentario) => {
-    const li = document.createElement("li");
-    li.className = "item-comentarios";
-    li.innerHTML = `
-    <p class=text-comentarios">${comentarios.comentarios}</p>
-    <small class="meta-comentarios">${comentario.nombre} . ${new Date(
-        comentario
-    ).toLocaleString()}</small>
+    lista.innerHTML = "";
+    comentarios.forEach((comentario) => {
+      const li = document.createElement("li");
+      li.className = "item-comentarios";
+      li.innerHTML = `
+        <p class="text-comentario">${comentario.comentario}</p>
+        <small class="meta-comentario">${comentario.nombre} • ${new Date(
+        comentario.fecha
+      ).toLocaleString()}</small>
     `;
-    lista.appendChild(li);
-});
-};
 
-mostrarComentarios();
+      lista.appendChild(li);
+    });
+  };
 
-formulario.addEventListener("submit", (e) => {
+  mostrarComentarios();
+
+  formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    
-    const datosFormulario =new FormData(formulario);
+
+    const datosFormulario = new FormData(formulario);
     const nombre = datosFormulario.get("nombre").trim();
-    const correo = datosFormulario.get("comentario").trim();
+    const correo = datosFormulario.get("correo").trim();
     const comentario = datosFormulario.get("comentario").trim();
 
     if (!comentario) return;
 
-    const comentarios = cargarComentarios ();
-    comentarios.push({nombre, correo, comentario, fecha: Date.now()});
-    guardarComentarios(comentarios);
-    mostrarComentarios();
-    formulario.reset();
-
-    EMAILJS_PUBLIC_KEY.send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
-        post_id: publicacionesID,
+    emailjs
+      .send(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, {
+        post_id: publicacionID,
         from_name: nombre,
         from_email: correo,
-        message:comentario,
-    })
-    .then(
+        message: comentario,
+      })
+      .then(
         () => {
-            agregarTost({
-                tipo:"existo",
-                titulo:"¡Comentario enviado!",
-                descripcion: "Tu comentario fue enviadocorrectamente.",
-                autoCierre: true,
-            });
+          const comentarios = cargarComentarios();
+          comentarios.push({ nombre, correo, comentario, fecha: Date.now() });
+          guardarComentarios(comentarios);
+          mostrarComentarios();
+          formulario.reset();
+
+          agregarToast({
+            tipo: "exito",
+            titulo: "¡Comentario enviado!",
+            descripcion: "Tu comentario fue enviado correctamente.",
+            autoCierre: true,
+          });
         },
         (err) => {
-            console.error("Error al enviar el comentario:", err);
-            agregarToast({
-                tipo: "error",
-                titulo: "Error al enviar comentario",
-                descripcion:
-                "Hubo un problemaal enviar tu comentario. Por favor, intétalo de nuevo más tarde.",
-                autoCierre: true,
-            });
+          console.error("Error al enviar el comentario:", err);
+          agregarToast({
+            tipo: "error",
+            titulo: "Error al enviar comentario",
+            descripcion:
+              "Hubo un problema al enviar tu comentario. Por favor, intétalo de nuevo más tarde.",
+            autoCierre: true,
+          });
         }
-    );
-});
+      );
+  });
 });
