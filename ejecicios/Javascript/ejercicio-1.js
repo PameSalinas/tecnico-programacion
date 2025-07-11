1978e98734fa3182b65fa4ffb0253bc00c9d8f4b
PameSalinas <salinascrisostomopamelaelizabe@gmail.com>
2025-07-11 16:04:08 -0600

    Ejercicios de Javascript agregados

---
+++ ejecicios/Javascript/ejercicio-1.js
@@ -0,0 +1,46 @@
+let miNombre = "Pamela";
+console.log(miNombre);
+
+let cantidadEmpleado = 20;
+console.log(cantidadEmpleado);
+
+let precioProducto = 15.99;
+console.log(precioProducto);
+
+let esActivo = true;
+esActivo = false;
+console.log(esActivo);
+
+let numero1 = 34;
+let numero2 = 1000;
+
+console.log(numero1 != numero2);
+
+let cantidadComputadoras;
+console.log(typeof cantidadComputadoras);
+
+let amigos = [
+    "Juan", 
+    "Ana",
+    "Pedro"
+];
+
+console.log(amigos[2]);
+
+const variedad = [
+    "Teléfono",
+    678,
+    true,
+    null,
+    ["Tablet", "Portátil", "Smartwatch"],
+    { marca: "Apple", modelo: "iPhone 13" }
+]
+
+console.log(variedad[5])
+
+const user = {
+    name: "Pamela",
+    age: 18
+}
+
+console.log(user.age);
