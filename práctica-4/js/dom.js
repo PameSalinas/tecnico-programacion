5925bc5d645747128d4a74284d92da5dd0dbfa78
PameSalinas <salinascrisostomopamelaelizabe@gmail.com>
2025-08-19 20:36:20 -0600

    practicaa agregada

--- práctica-4/js/dom.js
+++ práctica-4/js/dom.js
@@ -1,10 +1,22 @@
-export  const renderTable = (employees, tableBody) =>{
-    tableBody.innerHTML = "";
+export const renderTable = (employees, tableBody) => {
+  tableBody.innerHTML = "";
 
-    if (emplooyees.length !== 0)
-        employees.forEach((emp) => {
-    const row = document.createElement("tr");
-    row,innertHTML = `
-
-})
-}
+  if (emplooyees.length !== 0) {
+    employees.forEach((emp) => {
+      const row = document.createElement("tr");
+      row,
+        (innertHTML = `
+    <td>${emp.id}</td>
+    <td>${emp.name}</td>
+    <td>${emp.position}</td>
+    <td>${emp.department}</td>
+    <td>
+    <button data-action="edit" data-id="${emp.id}">Edit</button>
+    <button class="danger" data-action="delete" data-id"${emp.id}">Delete</button>
+    </td>
+    `);
+      row.style.animation = "fadeIn s ease";
+      tableBody.apendChild(row);
+    });
+  }
+};
