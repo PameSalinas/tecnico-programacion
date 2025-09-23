ef293cb463fee28a754d996cc6b086fcda735ccf
PameSalinas <salinascrisostomopamelaelizabe@gmail.com>
2025-09-22 19:02:08 -0600

    Estilos y pestaña agregados

--- proyecto-final/js/views/about.js
+++ proyecto-final/js/views/about.js
@@ -2,7 +2,18 @@ import Layout from "./layout.js";
 
 function aboutPage() {
   Layout(`
-    <h1>Acerca de</h1>
+    <section class="about-section">
+    <h1>Acerca de<h1/>
+    <p>Este sitio web está dedicado a explorar los luagres turisticos más facinantes de El Salvador. Aquí encontrarás una colección visual y descriptiva de destinos únicos, desde playas escondidas hasta pueblos con historia. </p>
+
+    <p>¿Qué hay para descubrir aquí?</p>
+    <ul>
+    <li>Playas secretas</li>
+    <li>Pueblos coloniales</li>
+    <li>Montañas y volcanes</li>
+    <li>Parques Acuáticos</li>
+    </ul>
+    </section>   
   `);
 }
 
